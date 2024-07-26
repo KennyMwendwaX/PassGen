94a103f6bf35f9d0f0bc385ac90c09f7a0f15623
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-07-26 10:35:46 +0300

    feat: add input validation

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
-	"time"
+	"math/big"
 
+	"github.com/atotto/clipboard"
 	"github.com/spf13/cobra"
 )
 
@@ -15,44 +16,79 @@ const (
 )
 
 var (
-	rng         *rand.Rand
 	length      int
 	useLetters  bool
 	useNumbers  bool
 	useSpecials bool
+	copyToClip  bool
+	verbose     bool
 )
 
 func init() {
-	// Create a new random generator with a random seed
-	rng = rand.New(rand.NewSource(time.Now().UnixNano()))
-
-	// Initialize the root command
-	rootCmd.Flags().IntVarP(&length, "length", "l", 16, "Length of the password")
+	rootCmd.Flags().IntVarP(&length, "length", "l", 16, "Length of the password (8-128)")
 	rootCmd.Flags().BoolVarP(&useLetters, "letters", "t", true, "Include letters")
 	rootCmd.Flags().BoolVarP(&useNumbers, "numbers", "n", true, "Include numbers")
 	rootCmd.Flags().BoolVarP(&useSpecials, "specials", "s", false, "Include special characters")
+	rootCmd.Flags().BoolVarP(&copyToClip, "copy", "c", false, "Copy to clipboard")
+	rootCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Verbose output")
 }
 
-func randString(n int, charset string) string {
+func secureRandomString(n int, charset string) (string, error) {
 	b := make([]byte, n)
 	for i := range b {
-		b[i] = charset[rng.Intn(len(charset))]
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			return "", err
+		}
+		b[i] = charset[num.Int64()]
 	}
-	return string(b)
+	return string(b), nil
 }
 
-func generatePassword(length int, useLetters, useNumbers, useSpecials bool) string {
-	charset := ""
+func generatePassword(length int, useLetters, useNumbers, useSpecials bool) (string, error) {
+	if length < 8 || length > 128 {
+		return "", fmt.Errorf("password length must be between 8 and 128")
+	}
+
+	if !useLetters && !useNumbers && !useSpecials {
+		return "", fmt.Errorf("at least one character type must be selected")
+	}
+
+	var charset string
+	var requiredChars []byte
+
 	if useLetters {
 		charset += letterBytes
+		requiredChars = append(requiredChars, letterBytes[0])
 	}
 	if useNumbers {
 		charset += numberBytes
+		requiredChars = append(requiredChars, numberBytes[0])
 	}
 	if useSpecials {
 		charset += specialBytes
+		requiredChars = append(requiredChars, specialBytes[0])
 	}
-	return randString(length, charset)
+
+	password, err := secureRandomString(length-len(requiredChars), charset)
+	if err != nil {
+		return "", err
+	}
+
+	for _, char := range requiredChars {
+		position, err := rand.Int(rand.Reader, big.NewInt(int64(len(password)+1)))
+		if err != nil {
+			return "", err
+		}
+		password = password[:position.Int64()] + string(char) + password[position.Int64():]
+	}
+
+	return password, nil
+}
+
+func calculateEntropy(password string) float64 {
+	charset := float64(len(letterBytes) + len(numberBytes) + len(specialBytes))
+	return float64(len(password)) * (float64(len(password)) / charset)
 }
 
 var rootCmd = &cobra.Command{
@@ -60,8 +96,27 @@ var rootCmd = &cobra.Command{
 	Short: "PasswordGen is a simple password generator",
 	Long:  `A simple command line tool to generate secure passwords.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		password := generatePassword(length, useLetters, useNumbers, useSpecials)
+		password, err := generatePassword(length, useLetters, useNumbers, useSpecials)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
 		fmt.Printf("Generated Password: %s\n", password)
+
+		if verbose {
+			entropy := calculateEntropy(password)
+			fmt.Printf("Password entropy: %.2f bits\n", entropy)
+		}
+
+		if copyToClip {
+			err := clipboard.WriteAll(password)
+			if err != nil {
+				fmt.Println("Error copying to clipboard:", err)
+			} else {
+				fmt.Println("Password copied to clipboard.")
+			}
+		}
 	},
 }
 
